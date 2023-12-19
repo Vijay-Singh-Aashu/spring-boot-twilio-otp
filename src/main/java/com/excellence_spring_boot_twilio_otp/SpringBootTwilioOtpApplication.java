e62b966d650ed07f7746c8acedaf8322eaff5fa4
Vijay Singh <vijaysinghaashu@gmail.com>
2023-12-19 16:43:40 +0530

    initializing the project

---
+++ src/main/java/com/excellence_spring_boot_twilio_otp/SpringBootTwilioOtpApplication.java
@@ -0,0 +1,13 @@
+package com.excellence_spring_boot_twilio_otp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTwilioOtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTwilioOtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence_spring_boot_twilio_otp/SpringBootTwilioOtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence_spring_boot_twilio_otp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTwilioOtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
